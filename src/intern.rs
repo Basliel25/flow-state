ef929fa56779266d52bf5d763f262f3441207cf0
Ras Bas <basgug25@gmail.com>
2026-05-30 14:23:09 +0200

    Initialize flowstate, add intern method to insert an index into the table

---
+++ src/intern.rs
@@ -0,0 +1,25 @@
+///! Flow key: The intern table - Maps flowkey state - u64
+///! Data held in an IndexMap 
+
+use indexMap::IndexMap;
+use crate::key::FlowKey;
+
+#[derive(Debug, Default)]
+pub struct FlowState {
+    table: IndexMap<FlowKey, ()>,
+}
+
+impl FlowState {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Intern as a key, returns its stable id
+    /// indexing to the same key returns the same id
+    pub fn intern(&mut self, key: FlowKey) -> u64 {
+        let (idx, _) = self.table.insert_full(key, ());
+        idx as u64
+    }
+
+
+}
