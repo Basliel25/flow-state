3ccb3f2406e3ccdeb3a2291b4a50b2e4f91bdd91
Ras Bas <basgug25@gmail.com>
2026-06-04 13:42:49 +0200

    Refactor procedere, first fit using FlowStateBuilder and predict/resolve with FlowState

--- src/state.rs
+++ src/state.rs
@@ -9,33 +9,40 @@ use crate::features::FlowFeatures;
 use crate::scaler::Scaler;
 use crate::cluster::ClusterModel;
 
-pub struct FlowState {
+pub struct FlowStateBuilder {
     k: usize, 
-    scaler: Option<Scaler>,
-    cluster: Option<ClusterModel>,
 }
 
-impl FlowState {
-    pub fn new(k: usize, cluster: ClusterModel) -> Self {
-        Self {k, 
-            scaler: None,
-            cluster: None}
+
+impl FlowStateBuilder {
+    pub fn new(k: usize) -> Self {
+        Self { k }
     }
 
     /// Fit scaler on raw batch, then fit cluster on scaled batch
-    pub fn fit(&mut self, batch: &[FlowFeatures]) {todo!()}
+    pub fn fit(&mut self, batch: &[FlowFeatures]) -> FlowState {todo!()}
+    pub fn k(&self)->usize {self.k}
+}
 
+/// Fitted modle
+pub struct FlowState {
+    k: usize,
+    scaler: Scaler,
+    cluster: ClusterModel,
+}
+
+impl FlowState {
+    // Construction happens through FlowStateBuilder::fit
+    
     /// Map a flow to its cluster id
     pub fn predict(&self, f: &FlowFeatures) -> u64 {todo!()}
 
-    ///Look up a centroid by cluster id
+    /// Look up a centroid by cluster id
     /// Look up is in the scaled space
     pub fn resolve(&self, id: u64) -> Array1<f64> {
         todo!()
     }
 
     pub fn k(&self)->usize {self.k}
-
 }
-
 
